1de40fcfd1a04dbda2c898f0e09b95b2d3d00167
DESKTOP-MVES09M\ayisc <ayiscoo@yahoo.com>
2019-02-17 17:11:11 +0100

    Intit Commit

---
+++ server/server.js
@@ -0,0 +1,21 @@
+
+const express = require('express');
+const path = require('path');
+
+//console.log(__dirname + '/../public');
+// path to solve path solutions  / nodejs module ..no installations
+ const publicPath = path.join(__dirname + '/../public');
+ console.log(publicPath);
+ var app = express();
+
+ var port = process.env.PORT || 3000;
+
+app.use(express.static(publicPath));
+
+app.listen(3000,()=>{
+    console.log(`connected to port ${port}`);
+ });
+
+//console.log( publicPath );
+
+  
